1405bb7587586e2bf30d03150cfa39156a9f3e28
Miura, Rai | Rai | TMO <rai.a.miura@rakuten.com>
2023-05-11 16:43:39 +0900

    insert db demodata

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,12 @@ from flask_smorest import Api
 from db import db
 
 import models
+import os
+from models.hotel import HotelModel
 
 from resources.hotel import blp as HotelBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Hotels REST API"
@@ -18,8 +20,21 @@ def create_app():
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    
     api = Api(app)
+    
+    with app.app_context():
+        db.create_all()
+        
+        hotel1 = HotelModel(hotel_id=1 , name='DisneyHotel', description='greate hotel', longitude=14.55, latitude=16.31, image="https://m.media-amazon.com/images/I/51V680RSx2L._AC_SY445_.jpg", prefecture="千葉")
+        db.session.add(hotel1)
+        db.session.commit()
 
     api.register_blueprint(HotelBlueprint)
+
 
     return app
--- backend/resources/hotel.py
+++ backend/resources/hotel.py
@@ -2,76 +2,19 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
 
 from db import db
+from models.hotel import HotelModel
 
 blp = Blueprint("Hotels", "hotels", description="Operations on hotels")
 
 
-@blp.route("/hotel/<string:hotel_id>")
+@blp.route("/hotel")
 class Hotel(MethodView):
-    def get(self, hotel_id):
+    def get(self):
         try:
-            return "Hello"
+            return "hello"
         except KeyError:
             abort(404, message="Hotel not found.")
-
-    def delete(self, item_id):
-        try:
-            del hotels[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
-
-    def put(self, item_id):
-        item_data = request.get_json()
-        # There's  more validation to do here!
-        # Like making sure price is a number, and also both hotels are optional
-        # Difficult to do with an if statement...
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
-        try:
-            item = hotels[item_id]
-
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
-
-
-@blp.route("/item")
-class ItemList(MethodView):
-    def get(self):
-        return {"hotels": list(hotels.values())}
-
-    def post(self):
-        item_data = request.get_json()
-        # Here not only we need to validate data exists,
-        # But also what type of data. Price should be a float,
-        # for example.
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
-        for item in hotels.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        hotels[item_id] = item
-
-        return item
