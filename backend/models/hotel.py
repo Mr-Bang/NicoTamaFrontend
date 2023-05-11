b628ed2d48b44fe323d3f8d9643385e7d6d17e64
Miura, Rai | Rai | TMO <rai.a.miura@rakuten.com>
2023-05-11 14:43:32 +0900

    first api

---
+++ backend/models/hotel.py
@@ -0,0 +1,13 @@
+from db import db
+
+
+class HotelModel(db.Model):
+    __tablename__ = "hotels"
+
+    hotel_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(240))
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    image = db.Column(db.String(240))
+    prefecture = db.Column(db.String(80))
---
+++ backend/resources/hotel.py
@@ -0,0 +1,77 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+from db import db
+
+blp = Blueprint("Hotels", "hotels", description="Operations on hotels")
+
+
+@blp.route("/hotel/<string:hotel_id>")
+class Hotel(MethodView):
+    def get(self, hotel_id):
+        try:
+            return "Hello"
+        except KeyError:
+            abort(404, message="Hotel not found.")
+
+    def delete(self, item_id):
+        try:
+            del hotels[item_id]
+            return {"message": "Item deleted."}
+        except KeyError:
+            abort(404, message="Item not found.")
+
+    def put(self, item_id):
+        item_data = request.get_json()
+        # There's  more validation to do here!
+        # Like making sure price is a number, and also both hotels are optional
+        # Difficult to do with an if statement...
+        if "price" not in item_data or "name" not in item_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+            )
+        try:
+            item = hotels[item_id]
+
+            # https://blog.teclado.com/python-dictionary-merge-update-operators/
+            item |= item_data
+
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
+
+
+@blp.route("/item")
+class ItemList(MethodView):
+    def get(self):
+        return {"hotels": list(hotels.values())}
+
+    def post(self):
+        item_data = request.get_json()
+        # Here not only we need to validate data exists,
+        # But also what type of data. Price should be a float,
+        # for example.
+        if (
+            "price" not in item_data
+            or "store_id" not in item_data
+            or "name" not in item_data
+        ):
+            abort(
+                400,
+                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+            )
+        for item in hotels.values():
+            if (
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
+            ):
+                abort(400, message=f"Item already exists.")
+
+        item_id = uuid.uuid4().hex
+        item = {**item_data, "id": item_id}
+        hotels[item_id] = item
+
+        return item
