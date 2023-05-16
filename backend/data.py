48b301dbe5a781ec1f50894a8504033ab900fb15
Kamata, Yuya | Yuya | TMO <yuya.kamata@rakuten.com>
2023-05-16 09:08:39 +0900

    add activities data

--- backend/data.py
+++ backend/data.py
@@ -14,3 +14,24 @@ hotels = [
         "region": "東部"
     }
 ]
+
+activities = [
+    {
+        "name": "Kawakudari",
+        "price": 8500,
+        "image": "http://......",
+        "longitude": 16.0,
+        "latitude": 2.0,
+        "url": "http:/.....",
+        "region": "都心"
+    },
+    {
+        "name": "concert",
+        "price": 10000,
+        "image": "http://......",
+        "longitude": 3.0,
+        "latitude": 3.0,
+        "url": "http:/.....",
+        "region": "副都心"
+    }
+]
