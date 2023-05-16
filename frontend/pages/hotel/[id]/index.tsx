98af766c7e25aab5d05dab399b654491d2411852
Higasa <koji.higasa@jp-gnwpw4mqhq.intra.rakuten.co.jp>
2023-05-16 09:12:30 +0900

    add disabled button

--- frontend/pages/hotel/[id]/index.tsx
+++ frontend/pages/hotel/[id]/index.tsx
@@ -25,6 +25,24 @@ function Page() {
       <Group position="center">
         <Button
 	  variant="default"
+          data-disabled
+          sx={{ '&[data-disabled]': { pointerEvents: 'all' } }}
+          onClick={(event) => event.preventDefault()}
+          styles={(theme) => ({
+            root: {
+              backgroundColor: '#EAEAEA',
+              border: 0,
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+          })}>施設紹介</Button>
+
+        <Button
+	  variant="default"
 	  component="a"
           target="_blank"
           rel="noopener noreferrer"
@@ -39,17 +57,115 @@ function Page() {
 		border: 0,
               }),
             },
-
             leftIcon: {
               marginRight: theme.spacing.md,
             },
-          })}>施設紹介</Button>
-        <Button variant="default">プラン一覧</Button>
-        <Button variant="default">部屋一覧</Button>
-        <Button variant="default">写真・動画(76)</Button>
-        <Button variant="default">地図・アクセス</Button>
-        <Button variant="default">お客様の声(567)</Button>
-        <Button variant="default">クーポン一覧</Button>
+	  })}>プラン一覧</Button>
+
+        <Button
+	  variant="default"
+	  component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://travel.rakuten.co.jp/HOTEL/80762/80762.html"
+          styles={(theme) => ({
+            root: {
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+              '&:not([data-disabled])': theme.fn.hover({
+                backgroundColor: theme.fn.darken('#EAEAEA', 0.05),
+		border: 0,
+              }),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+	  })}>部屋一覧</Button>
+
+        <Button
+	  variant="default"
+	  component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://travel.rakuten.co.jp/HOTEL/80762/80762.html"
+          styles={(theme) => ({
+            root: {
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+              '&:not([data-disabled])': theme.fn.hover({
+                backgroundColor: theme.fn.darken('#EAEAEA', 0.05),
+		border: 0,
+              }),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+	  })}>写真・動画(76)</Button>
+
+        <Button
+	  variant="default"
+	  component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://travel.rakuten.co.jp/HOTEL/80762/80762.html"
+          styles={(theme) => ({
+            root: {
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+              '&:not([data-disabled])': theme.fn.hover({
+                backgroundColor: theme.fn.darken('#EAEAEA', 0.05),
+		border: 0,
+              }),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+	  })}>地図・アクセス</Button>
+
+        <Button
+	  variant="default"
+	  component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://travel.rakuten.co.jp/HOTEL/80762/80762.html"
+          styles={(theme) => ({
+            root: {
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+              '&:not([data-disabled])': theme.fn.hover({
+                backgroundColor: theme.fn.darken('#EAEAEA', 0.05),
+		border: 0,
+              }),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+	  })}>お客様の声(567)</Button>
+
+        <Button
+	  variant="default"
+	  component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://travel.rakuten.co.jp/HOTEL/80762/80762.html"
+          styles={(theme) => ({
+            root: {
+              height: rem(42),
+              paddingLeft: rem(20),
+              paddingRight: rem(20),
+              '&:not([data-disabled])': theme.fn.hover({
+                backgroundColor: theme.fn.darken('#EAEAEA', 0.05),
+		border: 0,
+              }),
+            },
+            leftIcon: {
+              marginRight: theme.spacing.md,
+            },
+	  })}>クーポン一覧</Button>
       </Group>
     </MantineProvider>
   );
