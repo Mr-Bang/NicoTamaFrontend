f3227437283d07d9cccbee2d520824984123d073
Miura, Rai | Rai | TMO <rai.a.miura@rakuten.com>
2023-05-16 16:44:51 +0900

    create mappage

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { AppShell, MantineProvider } from '@mantine/core';
-import PageHeader from '@/components/PageHeader';
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { AppShell, MantineProvider } from "@mantine/core"
+import PageHeader from "@/components/PageHeader"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider  withGlobalStyles withNormalizeCSS>
-      <AppShell
-        padding="md"
-	header={<PageHeader />}
-      >
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <AppShell padding='md' header={<PageHeader />}>
         <Component {...pageProps} />
       </AppShell>
     </MantineProvider>
