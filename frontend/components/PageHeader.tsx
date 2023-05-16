973d78db58ad1c4a6c88aceeb26c2f7a1839e21a
Miura, Rai | Rai | TMO <rai.a.miura@rakuten.com>
2023-05-16 17:23:20 +0900

    fix marge bug

--- frontend/components/PageHeader.tsx
+++ frontend/components/PageHeader.tsx
@@ -21,50 +21,44 @@ export default function PageHeader() {
           <div className={classes.rakutenTravel}>
             <div className={classes.unnamed4}>楽天トラベル</div>
           </div>
-          <div className={classes.divRtServiceBar}>
-            <div className={classes._170_53Jpg}></div>
-            <div className={classes.a}>
-              <div className={classes.unnamed4}>楽天トラベル</div>
+
+          <div className={classes.divRtUtility}>
+            <div className={classes.ulRtHelpLink}>
+              <div className={classes.unnamed5}>楽天トラベルの使い方</div>
+              <div className={classes.unnamed6}>サイトマップ</div>
+              <div className={classes.unnamed7}>ヘルプ</div>
+              <div className={classes.languages}>Languages</div>
+              <div className={classes.pseudo}></div>
             </div>
-            <div className={classes.divRtUtility}>
-              <div className={classes.ulRtHelpLink}>
-                <div className={classes.unnamed5}>楽天トラベルの使い方</div>
-                <div className={classes.unnamed6}>サイトマップ</div>
-                <div className={classes.unnamed7}>ヘルプ</div>
-                <div className={classes.languages}>Languages</div>
-                <div className={classes.pseudo}></div>
+            <div className={classes.a2}>
+              <div className={classes.unnamed8}>予約の確認・キャンセル</div>
+            </div>
+            <div className={classes.dlRtNologin}>
+              <div className={classes.unnamed9}>ようこそ、楽天トラベルへ</div>
+              <div className={classes.unnamed10}>
+                <p className={classes.labelWrapper}>
+                  <span className={classes.label}>会員登録</span>
+                  <span className={classes.label2}> </span>
+                </p>
               </div>
-              <div className={classes.a2}>
-                <div className={classes.unnamed8}>予約の確認・キャンセル</div>
-              </div>
-              <div className={classes.dlRtNologin}>
-                <div className={classes.unnamed9}>ようこそ、楽天トラベルへ</div>
-                <div className={classes.unnamed10}>
-                  <p className={classes.labelWrapper}>
-                    <span className={classes.label}>会員登録</span>
-                    <span className={classes.label2}> </span>
-                  </p>
-                </div>
-                <div className={classes.unnamed11}>ログイン</div>
-              </div>
+              <div className={classes.unnamed11}>ログイン</div>
             </div>
           </div>
-          <div className={classes.dlRtNavLink}>
-            <div className={classes.ulRtNavBox}>
-              <div className={classes.a3}>
-                <div className={classes.unnamed12}>国内旅行</div>
-              </div>
-              <div className={classes.unnamed13}>国内ツアー</div>
-              <div className={classes.unnamed14}>レンタカー</div>
-              <div className={classes.unnamed15}>高速バス・バスツアー</div>
-              <div className={classes.unnamed16}>海外旅行</div>
-              <div className={classes.unnamed17}>遊び・体験</div>
-              <div className={classes.unnamed18}>割引クーポン</div>
-              <div className={classes.unnamed19}>ふるさと納税</div>
-              <div className={classes.unnamed20}>旅行ガイド</div>
+        </div>
+        <div className={classes.dlRtNavLink}>
+          <div className={classes.ulRtNavBox}>
+            <div className={classes.a3}>
+              <div className={classes.unnamed12}>国内旅行</div>
             </div>
+            <div className={classes.unnamed13}>国内ツアー</div>
+            <div className={classes.unnamed14}>レンタカー</div>
+            <div className={classes.unnamed15}>高速バス・バスツアー</div>
+            <div className={classes.unnamed16}>海外旅行</div>
+            <div className={classes.unnamed17}>遊び・体験</div>
+            <div className={classes.unnamed18}>割引クーポン</div>
+            <div className={classes.unnamed19}>ふるさと納税</div>
+            <div className={classes.unnamed20}>旅行ガイド</div>
           </div>
-          <div className={classes.x}>X</div>
         </div>
         <div className={classes.x}>X</div>
       </div>
