d4a0495fbbb2d08f5eba22989560ae1ecbffe070
Higasa <koji.higasa@jp-gnwpw4mqhq.intra.rakuten.co.jp>
2023-05-19 14:09:58 +0900

    arrange rooms

--- frontend/components/hotel/HotelContent.tsx
+++ frontend/components/hotel/HotelContent.tsx
@@ -1,6 +1,6 @@
 import { Hotel } from "@/types/hotel"
 import { RoomList } from "@/types/roomList"
-import { Box, Image, Grid, SimpleGrid } from "@mantine/core"
+import { Box, Badge, Button, Card, Image, Grid, Group, SimpleGrid, Text } from "@mantine/core"
 
 type Props = {
   hotel: Hotel
@@ -20,7 +20,32 @@ export default function SampleHotel(props: Props) {
       <SimpleGrid cols={2}>
         {roomList.map((room, index) => (
           <Grid.Col span={4} key={index}>
-            <Image radius='md' src={room.image} alt={room.room_number.toString()} />
+            <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                src={room.image}
+                height={260}
+		width={550}
+                alt={room.room_number.toString()}
+              />
+            </Card.Section>
+
+            <Group position="apart" mt="md" mb="xs">
+              <Text weight={500}>{room.room_number.toString()} 号室</Text>
+              { room.available ? <Badge color="blue" variant="light">
+	        空室あり
+              </Badge> : <Badge color="pink" variant="light">
+	        満室
+              </Badge> }
+            </Group>
+
+            <Text size="sm" color="dimmed">
+            </Text>
+
+            { room.available ? <Button variant="outline" fullWidth mt="md" radius="md">
+              予約する
+            </Button> : "" }
+            </Card>
           </Grid.Col>
         ))}
       </SimpleGrid>
