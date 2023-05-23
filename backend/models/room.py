a2f296543b0355356d163b31437db239d5123654
Kamata, Yuya | Yuya | TMO <yuya.kamata@rakuten.com>
2023-05-23 16:44:19 +0900

    change room number into room type

--- backend/models/room.py
+++ backend/models/room.py
@@ -6,7 +6,7 @@ class RoomModel(db.Model):
     
     room_id = db.Column(db.Integer, primary_key=True)
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotels.hotel_id"), nullable=False)
-    room_number = db.Column(db.Integer)
+    room_type = db.Column(db.String)
     price = db.Column(db.Integer, nullable=False)
     available = db.Column(db.Boolean, nullable=False)
     image = db.Column(db.String(240))
